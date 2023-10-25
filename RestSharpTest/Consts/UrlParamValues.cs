7f17c29edacca656efb89f75a8c6efa37f07368a
Vlad Filip <vladfilip94@gmail.com>
2023-10-25 09:12:16 +0300

    Updated version

--- RestSharpTest/Consts/UrlParamValues.cs
+++ RestSharpTest/Consts/UrlParamValues.cs
@@ -1,26 +1,28 @@
 ﻿using RestSharp;
+using RestSharpTest.Exceptions;
+using RestSharpTest.Tests;
 
 
 namespace RestSharpTest.Consts;
 
-public class UrlParamValues
+public class UrlParamValues : TestCore
 {
     public const string ExistingBoardId = "650a79acfdb0b2b2517e5c77";
     public const string UserName = "me";
-    public const string ValidKey = "7f9cad4066e9b4e921810882d7d0fc01";
-    public const string ValidToken = "ATTAa6e2128ae73c22c88b2460e708691398d59b4b8425b044d6918568f451ed7c1b1866383B";
     public const string BoardIdToUpdate = "652040cf2ca93d01247b8170";
-
+    public readonly string Validtkey = AuthData.ValidKey;
+    public readonly string ValidToken = AuthData.ValidToken;
+    
     public static readonly IEnumerable<Parameter> AuthQueryParams = new[]
     {
-         Parameter.CreateParameter("key", ValidKey, ParameterType.QueryString),
-        Parameter.CreateParameter("token", ValidToken, ParameterType.QueryString)
+         Parameter.CreateParameter("key", AuthData.ValidKey, ParameterType.QueryString),
+        Parameter.CreateParameter("token",  AuthData.ValidToken, ParameterType.QueryString)
     };
     
     public static readonly IEnumerable<Parameter> AnotherUserAuthQueryParams = new[]
     {
-        Parameter.CreateParameter("key", "7f9cad4066e9b4e921810882d7d0fc01", ParameterType.QueryString),
-        Parameter.CreateParameter("token", "384rehfkodsjfpsaujf304u93u4932u4932yr98er39y493u49", ParameterType.QueryString)
+        Parameter.CreateParameter("key", AuthData.ValidKey , ParameterType.QueryString),
+        Parameter.CreateParameter("token", AuthData.ValidToken, ParameterType.QueryString)
     };
 
 }
---
+++ RestSharpTest/Tests/TestCore.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using RestSharpTest.Exceptions;
+using RestSharpTest.Options;
+using RestSharpTest.Services;
+
+namespace RestSharpTest.Tests;
+
+public class TestCore : RestSharpExceptions
+{
+    public static IConfigurationRoot Configuration { get; private set; }
+    public static ProjectSettings ProjectSettings { get; private set; }
+    public static AuthParams AuthData { get; private set; }
+    
+    [OneTimeSetUp]
+    public  void SetUpConfigurations()
+    {
+        var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+        
+        Configuration = builder.Build();
+        ProjectSettings = Configuration.GetSection("ProjectSettings").Get<ProjectSettings>();
+        
+        var validKey = Environment.GetEnvironmentVariable("ValidKey", EnvironmentVariableTarget.Process);
+        var validToken = Environment.GetEnvironmentVariable("ValidToken", EnvironmentVariableTarget.Process);
+
+        if (string.IsNullOrEmpty(validKey))
+            throw new ConfigurationNotFoundException("Environment Variable ValidKey not found");
+
+        if (string.IsNullOrEmpty(validToken))
+            throw new ConfigurationNotFoundException("Environment Variable ValidToken not found");
+
+        AuthData = new AuthParams()
+        {
+            ValidKey = validKey,
+            ValidToken = validToken
+        };
+    }
+}
